12a14a27a555237686ea71fe9eefbd6e5c7140be
Richard Parayno <broodheart@gmail.com>
2016-10-10 13:56:00 +0800

    Added gitignore

--- TestBot.php
+++ TestBot.php
@@ -1,8 +1,6 @@
 <?php
+use Telegram\Bot\Api;
 
-define('BOT_TOKEN', '225644089:AAG3cbFX1yskc5aA-EKxjuV_0PF25d_GPZs');
-define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
-
-
+$telegram = new Api('225644089:AAG3cbFX1yskc5aA-EKxjuV_0PF25d_GPZs');
 
  ?>
